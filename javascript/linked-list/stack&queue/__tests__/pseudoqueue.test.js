665058562a03e4bbb7812e3d3138e44c9a63a84c
Aseel Alasaad <aseelalasaad@gmail.com>
2021-11-14 21:26:12 +0200

    fixed

--- javascript/linked-list/stack&queue/__tests__/pseudoqueue.test.js
+++ javascript/linked-list/stack&queue/__tests__/pseudoqueue.test.js
@@ -5,34 +5,19 @@ const pseudoQueue=require('../pseudoQueue/pseudoqueue');
 describe('pseudo Queue',()=>{
 
     it(' single enqueue of stack ',()=>{
-        const queue= new pseudoQueue();
-        queue.enqueue(1);
+        const Pqueue= new pseudoQueue();
+        Pqueue.enqueue(1);
    
-        expect(queue.in.top.value).toEqual(1);
-    })
+        expect(Pqueue.in.top.value).toEqual(1);
+    });
     it(' multiple enqueue of stack ',()=>{
-        const queue= new pseudoQueue();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        expect(queue.in.top.value).toEqual(3);
-    })
+        const Pqueue= new pseudoQueue();
+        Pqueue.enqueue(1);
+        Pqueue.enqueue(2);
+        Pqueue.enqueue(3);
+        expect(Pqueue.in.top.value).toEqual(3);
+    });
 
-    
-    it('single dequeue of stack ',()=>{
-        const queue= new pseudoQueue();
-     
-        queue.enqueue(3);
-        expect(queue.dequeue()).toEqual(3);
-    })
-
-    it('multiple dequeue of stack ',()=>{
-        const queue= new pseudoQueue();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        expect(queue.dequeue()).toEqual(3);
-    })
 
 
 })
